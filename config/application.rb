6086c315542d287af611ac2f981563163a13f431
knt930 <sword11wg@gmail.com>
2018-10-04 15:35:30 +0900

    アプリケーションのデフォルトの言語を日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Scoutter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
