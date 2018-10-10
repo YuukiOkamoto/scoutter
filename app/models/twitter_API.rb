69493612e2c1064b26b6b791b6d350fd783dd71f
knt930 <sword11wg@gmail.com>
2018-10-10 19:46:08 +0900

    twitterAPIモデルを作成

---
+++ app/models/twitter_API.rb
@@ -0,0 +1,17 @@
+require 'twitter'
+require 'singleton'
+
+class TwitterAPI
+  include Singleton
+
+  attr_reader :client
+
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.access_token = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+  end
+end
