7c138af081f1243980ae790f192f5ecf7925c4e5
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-10 16:32:31 +0900

    トータル戦闘力から該当キャラクターを割り出すクラスメソッドを作成

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,20 @@ class Character < ApplicationRecord
   validates :minimum, presence: true
   validates :maximum, presence: true
   validates :growth_rate, presence: true
+
+  scope :power_range, -> { select(:id, :minimum, :maximum) }
+
+  class << self
+    def character_decision(total_power)
+      get_ranges.each do |r|
+        if total_power.between?(r.minimum, r.maximum)
+          return r.id
+        end
+      end
+    end
+
+    def get_ranges
+      Character.select(:id, :minimum, :maximum).to_a
+    end
+  end
 end
