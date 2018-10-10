234308cac4196b7a5628dc99445349533a6c3d2f
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-10 16:33:11 +0900

    usersに追加したtotal_powerとcharacter_idのテストデータを追加

--- db/fixtures/development/users.rb
+++ db/fixtures/development/users.rb
@@ -3,5 +3,9 @@
     s.id = i
     s.name = 'user' + i.to_s
     s.twitter_id = i
+    s.total_power = PowerLevel.get_total_power(user_id: i)
+    s.character_id = Character.character_decision(
+      PowerLevel.where(user_id: i).sum(:power)
+    )
   end
 end
