bd4ebfda32f6468f6094bf437b137f8160dae5d9
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-10 16:31:39 +0900

    トータル戦闘力を算出するクラスメソッドを作成

--- app/models/power_level.rb
+++ app/models/power_level.rb
@@ -2,4 +2,10 @@ class PowerLevel < ApplicationRecord
   belongs_to :user
 
   validates :power, presence: true
+
+  class << self
+    def get_total_power(user_id: current_user&.id)
+      all.where(user_id: user_id).sum(:power)
+    end
+  end
 end
