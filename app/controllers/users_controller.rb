be2c0242048e496f0fe9ca9e2cadcd702fc296a1
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-12 03:42:47 +0900

    選択された期間によって、コントローラーでランキングを返す処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def rank
-    @rankings = User.power_total_rank.page(params[:page]).per(25)
+    case @period = params[:period] || 'total'
+    when 'total'
+      @ranks = User.power_rank.total_period.page(params[:page]).per(25)
+    when 'week'
+      @ranks = User.power_rank.week_period.page(params[:page]).per(25)
+    when 'day'
+      @ranks = User.power_rank.day_period.page(params[:page]).per(25)
+    end
   end
 end
