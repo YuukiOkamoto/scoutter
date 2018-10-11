eb80b37e33e939bdc61a80c4e8020396b5131ed4
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-12 03:41:02 +0900

    戦闘力の期間絞り込みのscopeを追加

--- app/models/power_level.rb
+++ app/models/power_level.rb
@@ -3,6 +3,9 @@ class PowerLevel < ApplicationRecord
 
   validates :power, presence: true
 
+  scope :weekly, -> { where(updated_at: (Date.today.ago(6.days))..(Time.current)) }
+  scope :daily, -> { where(updated_at: (Date.today)..(Time.current)) }
+
   class << self
     def get_total_power(user_id: current_user&.id)
       all.where(user_id: user_id).sum(:power)
