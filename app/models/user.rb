0b181e09347eab7d05184811ae0f1f821eba216b
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-12 01:43:44 +0900

    userとsum_powerのリレーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_one_attached :icon
 
+  has_many :sum_power
+
   has_many :power_levels
   has_many :authentications, dependent: :destroy
 
