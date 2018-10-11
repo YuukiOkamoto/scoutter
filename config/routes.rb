a0d533d9670d36dd1374143738b8a61fef839adf
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-11 20:44:26 +0900

    ランキング表示するルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/ranking', to: 'power_levels#rank'
+  get '/ranking', to: 'users#rank'
   root to: 'home#index'
   post 'oauth/callback' => 'oauths#callback'
   get 'oauth/callback' => 'oauths#callback'
