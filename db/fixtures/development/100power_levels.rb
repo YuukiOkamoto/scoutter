0e98dccb61a955bc303bc50abda6a71fb7d4d8b5
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-12 12:12:34 +0900

    戦闘力のテストデータをユーザー毎の幅を広げるよう変更

--- db/fixtures/development/100power_levels.rb
+++ db/fixtures/development/100power_levels.rb
@@ -2,6 +2,7 @@
   PowerLevel.seed(:id) do |s|
     s.id = i
     s.user_id = i % 100 + 1
-    s.power = i * (i % 100 + 1)
+    s.power = ((i * (i % 100 + 1)) / Rational(1000000, ((i % 100 + 1)**6))).to_i
+    s.updated_at = Time.current.ago(i**2)
   end
 end
