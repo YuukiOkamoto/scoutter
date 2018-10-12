36c28584f9d903170ad84d988059595849d25bf8
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-12 15:36:01 +0900

    メソッド名変更

--- db/fixtures/development/200users.rb
+++ db/fixtures/development/200users.rb
@@ -3,7 +3,7 @@
     s.id = i
     s.name = 'user' + i.to_s
     s.twitter_id = i
-    s.character_id = Character.character_decision(
+    s.character_id = Character.decide_character_id(
       PowerLevel.where(user_id: i).sum(:power)
     )
   end
