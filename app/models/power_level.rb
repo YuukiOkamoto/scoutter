c6c57899a701b679548d0a6ed258f7038bc052a3
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-14 11:31:00 +0900

    x日前以前の戦闘力の合計と、x日前の戦闘力の取得のscopeを追加

--- app/models/power_level.rb
+++ app/models/power_level.rb
@@ -5,6 +5,10 @@ class PowerLevel < ApplicationRecord
 
   scope :weekly, -> { where(updated_at: (Date.today.ago(6.days))..(Time.current)) }
   scope :daily, -> { where(updated_at: (Date.today)..(Time.current)) }
+  scope :until_ago, ->(count) { where('updated_at<=?', Time.zone.today.ago(count.days).end_of_day) }
+  scope :ago, ->(count) do
+    where(updated_at: Time.zone.today.ago(count.days).beginning_of_day..Time.zone.today.ago(count.days).end_of_day)
+  end
 
   class << self
     def get_total_power(user_id:)
