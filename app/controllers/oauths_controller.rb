bf1f73108b3a0359e5a7400d43e47eb0b0cb8b4b
knt930 <sword11wg@gmail.com>
2018-10-14 21:52:35 +0900

    Twitterログイン時のcallbackアクション内で、TwitterAPIから必要なデータを取得し、戦闘力としてデータベースに保存する処理を実装

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,19 +1,48 @@
 class OauthsController < ApplicationController
   skip_before_action :require_login, raise: false
+  before_action :set_uid
   def oauth
     login_at(params[:provider])
   end
 
-  # TODO: 下記アクションのリダイレクト先はログイン動作確認のための仮のパスなので本来の遷移先は戦闘力を表示するページになる予定
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
+      # favのスコア合計値
+      @fav = TwitterAPI.fav(@uid, current_user.id)
+
+      # retweetのスコア合計値
+      @retweet = TwitterAPI.retweet(@uid)
+
+      #quoteのスコア合計値
+      @quote = TwitterAPI.quote(@uid)
+
+      # replyのスコア合計値
+      @reply = TwitterAPI.reply(@uid)
+
+      # tweetのスコア合計値
+      @tweet = TwitterAPI.tweet(@uid)
+
+      # デイリーの合計スコアを算出し、デイリー戦闘力に換算
+      @daily_score = @fav + @retweet + @quote + @reply + @tweet
+      @daily_power = @daily_score * Character.find(current_user.character_id).growth_rate
+      @daily_power = @daily_power.to_i
+
+      # 1日の最新の戦闘力をpower_levelsテーブルに保存
+      # 今日一度も戦闘力を図っていなければ日時の戦闘力を作成し、一度でも測っていれば戦闘力を更新する
+      @daily_power_record = PowerLevel.where('created_at > ?', Time.now.beginning_of_day).find_by(user_id: current_user.id)
+      if @daily_power_record.nil?
+        PowerLevel.create(user_id: current_user.id, power: @daily_power)
+      else
+        @daily_power_record.power = @daily_power
+        @daily_power_record.save
+      end
+
       redirect_to power_levels_path
     else
       begin
         @user = create_from(provider)
         # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-        @uid = Authentication.find_by(user_id: @user.id).uid.to_i
         if TwitterAPI.instance.client.user(@uid).protected?
           Authentication.find_by(user_id: @user.id).destroy
           @user.destroy
@@ -22,10 +51,16 @@ class OauthsController < ApplicationController
         end
         reset_session # protect from session fixation attack
         auto_login(@user)
-        redirect_to power_levels_index_path
+        redirect_to power_levels_path
       rescue
         redirect_to root_path
       end
     end
+  end
+
+  private
+
+  def set_uid
+    @uid = Authentication.find_by(user_id: current_user.id).uid.to_i
   end
 end
--- app/models/twitter_API.rb
+++ app/models/twitter_API.rb
@@ -14,4 +14,80 @@ class TwitterAPI
       config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
   end
+
+  class << self
+    def fav(uid, user_id)
+      fav_point = ActionPoint.fav.point
+      total_fav = TwitterAPI.instance.client.user(uid).favorites_count
+      activity = Activity.find_by(user_id: user_id, action_id: 1)
+
+      if activity == nil
+        Activity.create(user_id: user_id, action_id: 1, yesterday_value: total_fav)
+        yesterday_fav = total_fav
+
+        @fav = fav_point * (total_fav - yesterday_fav)
+      else
+        yesterday_fav = Activity.find_by(user_id: user_id, action_id: 1).yesterday_value
+        @fav = fav_point * (total_fav - yesterday_fav)
+        activity.latest_value = total_fav
+        activity.save
+      end
+      @fav
+    end
+
+    def retweet(uid)
+      retweet_count = TwitterAPI.instance.client.retweeted_by_user(user_id: uid, count: 10).select { |tweet| tweet.created_at > Time.now.beginning_of_day }.count
+      retweet_point = ActionPoint.retweet.point
+      @retweet = retweet_point * retweet_count
+    end
+
+    def quote(uid)
+      quote = []
+
+      TwitterAPI.instance.client.user_timeline(user_id: uid, count: 10).select {|tweet| tweet.created_at > Time.now.beginning_of_day}.each do |tweet|
+        if tweet.quote?
+          quote << tweet
+        end
+      end
+      quote_count = quote.count
+      quote_point = ActionPoint.quote.point
+      @quote = quote_point * quote_count
+    end
+
+    def reply(uid)
+      reply_count = TwitterAPI.instance.client.user_timeline(user_id: uid, count: 50).select {|tweet| tweet.created_at > Time.now.beginning_of_day}.count - TwitterAPI.instance.client.user_timeline(uid, options = { count: 1000, exclude_replies: true }).select {|tweet| tweet.created_at > Time.now.beginning_of_day}.count
+      reply_point = ActionPoint.reply.point
+      @reply = reply_count * reply_point
+    end
+
+    def tweet(uid)
+      xs_tweet_count = 0
+      xs_tweet_point = ActionPoint.xs_tweet.point
+      s_tweet_count = 0
+      s_tweet_point = ActionPoint.s_tweet.point
+      l_tweet_count = 0
+      l_tweet_point = ActionPoint.l_tweet.point
+      xl_tweet_count = 0
+      xl_tweet_point = ActionPoint.xl_tweet.point
+      TwitterAPI.instance.client.user_timeline(user_id: uid, count: 50, exclude_replies: true, include_rts: false).select {|tweet| tweet.created_at > Time.now.beginning_of_day}.each do |tweet|
+        unless tweet.quote?
+          if tweet.text.length.between?(1, 14)
+            xs_tweet_count += 1
+          elsif tweet.text.length.between?(15, 49)
+            s_tweet_count += 1
+          elsif tweet.text.length.between?(50, 99)
+            l_tweet_count += 1
+          elsif tweet.text.length.between?(100, 140)
+            xl_tweet_count +=1
+          end
+        end
+      end
+      @xs_tweet = xs_tweet_count * xs_tweet_point
+      @s_tweet = s_tweet_count * s_tweet_point
+      @l_tweet = l_tweet_count * l_tweet_point
+      @xl_tweet = xl_tweet_count * xl_tweet_point
+
+      @tweet = @xs_tweet + @s_tweet + @l_tweet + @xl_tweet
+    end
+  end
 end
