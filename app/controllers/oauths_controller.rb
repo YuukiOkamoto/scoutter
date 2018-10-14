2ed327ff26c85d5c10a9f4851e833189b5abbef6
knt930 <sword11wg@gmail.com>
2018-10-14 17:00:17 +0900

    非公開ユーザーがログインしてきた際にルートページにフラッシュメッセージ付きでリダイレクトする処理を実装

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,15 +8,21 @@ class OauthsController < ApplicationController
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
-      redirect_to root_path
+      redirect_to power_levels_path
     else
       begin
         @user = create_from(provider)
         # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-
+        @uid = Authentication.find_by(user_id: @user.id).uid.to_i
+        if TwitterAPI.instance.client.user(@uid).protected?
+          Authentication.find_by(user_id: @user.id).destroy
+          @user.destroy
+          redirect_to root_path, danger: '申し訳ありません。非公開アカウントではログインできません。'
+          return
+        end
         reset_session # protect from session fixation attack
         auto_login(@user)
-        redirect_to root_path
+        redirect_to power_levels_index_path
       rescue
         redirect_to root_path
       end
