7f0a24f4839f4d4ab70e2f9c6c94e525e713adfc
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-18 16:07:27 +0900

    ヘッダー・ナビゲーションバーを作成

---
+++ app/helpers/tab_helper.rb
@@ -0,0 +1,21 @@
+module TabHelper
+  def displaying?(controller_name: nil, action_name: nil)
+    if controller_name == nil
+      check_action_name(action_name)
+    elsif action_name == nil
+      check_controller_name(controller_name)
+    else
+      check_controller_name(controller_name) && check_action_name(action_name)
+    end
+  end
+
+  private
+
+    def check_controller_name(controller_name)
+      controller.controller_name == controller_name
+    end
+
+    def check_action_name(action_name)
+      controller.action_name == action_name
+    end
+end
