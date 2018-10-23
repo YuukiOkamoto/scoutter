0f430d02cce245f188db45cc5168a1b12cdd8cea
knt930 <sword11wg@gmail.com>
2018-10-23 13:52:13 +0900

    イーシンチュウをホバーした際の戦闘力を測るボタンの色を変更する処理をaddclassに変更

--- app/assets/javascripts/db_button.js
+++ app/assets/javascripts/db_button.js
@@ -7,11 +7,7 @@ $('.db-btn').on('click', function(){
 });
 
 $('.db-btn').hover( () => {
-    $('.power-btn').css('color', '#fff');
-    $('.power-btn').css('background-color', '#d23430');
-    $('.power-btn').css('border-color', '#c9302c');
+    $('.power-btn').addClass('db-hover');
 }, function() {
-    $('.power-btn').css('color', '');
-    $('.power-btn').css('background-color', '');
-    $('.power-btn').css('border-color', '');
+    $('.power-btn').removeClass('db-hover');
 });
