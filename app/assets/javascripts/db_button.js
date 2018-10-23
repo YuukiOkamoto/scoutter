05f3681776d7707b59aad5e082fcbf18a7818af3
knt930 <sword11wg@gmail.com>
2018-10-23 12:17:01 +0900

    JSでイーシンチュウにホバーした時に戦闘力を測るボタンの色を変更する処理を追加

--- app/assets/javascripts/db_button.js
+++ app/assets/javascripts/db_button.js
@@ -5,3 +5,13 @@ $('.power-btn').on('click', function(){
 $('.db-btn').on('click', function(){
     $(this).addClass('db-spin');
 });
+
+$('.db-btn').hover( () => {
+    $('.power-btn').css('color', '#fff');
+    $('.power-btn').css('background-color', '#d23430');
+    $('.power-btn').css('border-color', '#c9302c');
+}, function() {
+    $('.power-btn').css('color', '');
+    $('.power-btn').css('background-color', '');
+    $('.power-btn').css('border-color', '');
+});
