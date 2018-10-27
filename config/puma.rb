d70a698a0a2957e2a4a22379cb358405058523f4
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-27 22:08:00 +0900

    socketに関するpumaの設定

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,11 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
+
+# socketの設定
+app_dir = File.expand_path("../..", __FILE__)
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
