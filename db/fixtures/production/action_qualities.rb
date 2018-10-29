af8d88eedcd612fa3618532ad57395366753ab7c
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-10-29 16:08:21 +0900

    誤字修正

--- db/fixtures/production/action_qualities.rb
+++ db/fixtures/production/action_qualities.rb
@@ -3,7 +3,7 @@ ActionQuality.seed(
   { id: 1, action_id: 1, quality: 10 },
   { id: 2, action_id: 2, quality: 10 },
   { id: 3, action_id: 3, quality: 10 },
-  { id: 4, action_id: 4, mquality: 10 },
+  { id: 4, action_id: 4, quality: 10 },
   { id: 5, action_id: 5, minimum: 1, maximum: 14, quality: 5 },
   { id: 6, action_id: 5, minimum: 15, maximum: 49, quality: 10 },
   { id: 7, action_id: 5, minimum: 50, maximum: 99, quality: 15 },
