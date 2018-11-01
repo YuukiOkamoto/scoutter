80ca258c1247fa0cb637e5b5c3a45c77b3646b98
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-11-01 18:06:15 +0900

    サイト名変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def default_meta_tags
     {
       og: {
-        site_name: 'Scoutter',
+        site_name: 'Scoutter 〜Twitter戦闘力診断〜',
         description: 'あなたの戦闘力を測ってみよう！',
         'format-detection' => 'telephone=no,address=no,email=no',
         image: "#{asset_url('top.png')}",
