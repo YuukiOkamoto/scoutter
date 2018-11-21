c81034bdccdd5f36726373ccba34956902e81e40
yuukiokamoto <yuukiokamoto2fc@gmail.com>
2018-11-21 16:38:12 +0900

    Actionモデルの不要なバリデーション削除

--- app/models/action.rb
+++ app/models/action.rb
@@ -3,9 +3,6 @@ class Action < ApplicationRecord
   has_many :action_points, through: :action_qualities
   has_many :activities, dependent: :destroy
 
-  validates :name, presence: true
-  validates :twitter_id, presence: true
-
   enum kind: { fav: 1, retweet: 2, quote: 3, reply: 4, tweet: 5 }
 
   class << self
